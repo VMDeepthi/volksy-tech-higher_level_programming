e8df7f7e9cfa803a703c33f148a0493dc2a8f2bb
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-02 05:08:25 -0800

    TASK8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     """Print a string in uppercase."""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-            print("{}".format(c), end="")
-    print("")
+    res = ''
+    for i in str:
+        res = res + chr(ord(i) - 32)
+    return res
