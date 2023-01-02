97c226776685706b47b34662291948ffdda2018f
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-02 04:38:07 -0800

    TASK9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         last_digit = number % 10
     elif number < 0:
         last_digit = (number*-1)%10
-    print(last_digit,end='')
+    print(last_digit, end='')
     return last_digit
