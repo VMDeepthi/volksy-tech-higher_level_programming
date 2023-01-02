3d4ba9a6b98a97b4c99f0587b82e0d249a9d91cd
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-02 04:00:53 -0800

    Task8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     new_str = ""
     for i in range(len(str)):
-        if(ord(str[i]) >= 97 and ord(str[i]) <= 122):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
             new_str += chr(ord(str[i]) - 32)
             continue
         new_str += str[i]
