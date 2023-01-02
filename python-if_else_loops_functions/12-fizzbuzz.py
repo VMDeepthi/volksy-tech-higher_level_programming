da41ee7888118f066a18e45e55eab69609b2780a
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-02 08:22:10 -0800

    Task12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizbuzz():
-for fizzbuzz in range(51):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
+    for a in range(51):
+        if a % 5 == 0 and a % 3 == 0:
+            print("Fizzbuzz", end=" ")
+            continue
+        elif a % 5 == 0:
+             print("Buzz", end=" ")
+             continue
+        elif a % 3 == 0:
+        print("Fizz", end=" "))
         continue
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-        continue
-    print(fizzbuzz)
+        print(a, end=" ")
