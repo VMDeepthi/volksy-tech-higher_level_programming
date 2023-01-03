f9f6f78b66c7327ce621fbdbc25b0553415050ef
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-03 09:09:43 -0800

    Task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if _name_ == "_main_":
     import sys
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum = sum + int(sys.argv[i])
-    print(sum)
+    length = len(sys.argv)
+    if length <= 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("{:d} argument:".format(length - 1))
+    else:
+        print("{:d} arguments:".format(length - 1))
+    for i in range(1, length):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
+    
