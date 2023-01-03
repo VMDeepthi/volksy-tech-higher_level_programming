9385e6aca83068c5b761b739032cbe5c545cfa3c
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-03 01:23:38 -0800

    Task0

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,4 +7,3 @@ print("{:d}+{:d}={:d}".format(a,b,add(a,b)))
 print("{:d}+{:d}={:d}".format(a,b,sub(a,b)))
 print("{:d}+{:d}={:d}".format(a,b,mul(a,b)))
 print("{:d}+{:d}={:d}".format(a,b,div(a,b)))
-
