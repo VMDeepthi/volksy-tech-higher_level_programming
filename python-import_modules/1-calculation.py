40480328c5e272df268b55d4456cf3493199245c
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-03 01:30:45 -0800

    Task1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if__name__ =="__main__"
-from add_0 import add,sub,mul,div
+from calculator_1.py import add,sub,mul,div
 a=1
 b=2
 print("{:d}+{:d}={:d}".format(a,b,add(a,b)))
-print("{:d}+{:d}={:d}".format(a,b,sub(a,b)))
-print("{:d}+{:d}={:d}".format(a,b,mul(a,b)))
-print("{:d}+{:d}={:d}".format(a,b,div(a,b)))
+print("{:d}-{:d}={:d}".format(a,b,sub(a,b)))
+print("{:d}*{:d}={:d}".format(a,b,mul(a,b)))
+print("{:d}/{:d}={:d}".format(a,b,div(a,b)))
--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def add(a, b):
     return a+b
+def sub(a, b):
     return a-b
+def mul(a, b):
     return a*b
+def division(a, b):
     return a/b
