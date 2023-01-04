aabe56d1224a90a6f8652246866fcd235d878195
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-04 02:12:34 -0800

    Task7

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
-
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
-
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-    argv[2], int(argv[3]), result)
-
+if __name__ == "__main__":
+    from calculator_1 import add, sub, div, mul
+    from sys import argv
+    if len(argv) != 4:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operator = argv[2]
+    n1 = int(argv[1])
+    n2 = int(argv[3])
+    if operator == "+":
+        print("{} + {} = {}".format(n1, n2, add(n1, n2)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(n1, n2, sub(n1, n2)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(n1, n2, mul(n1, n2)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(n1, n2, div(n1, n2)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
