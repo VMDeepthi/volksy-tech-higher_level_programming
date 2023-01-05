c12d1f644f8247f6f1d8fdc2f8919f1defd2dcb3
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-05 04:04:34 -0800

    Task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 def no_c(my_string):
-    lst=[]
+    lst =[]
     for x in my_string:
-        if x!= 'c' and x != 'C':
-            lst.append(x)       
+        if x! = 'c' and x != 'C':
+            lst.append(x)
     return("".join(lst))
-
