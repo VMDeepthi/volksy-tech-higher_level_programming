2a6c07a9887f785c8b531eaf010aa4bf3e72a77b
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-05 22:51:54 -0800

    Task6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            print("{:d}".format(i),end="" if i != row[-1] else " ")
+        print()        
