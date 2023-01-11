5d8be0e8a425b98a0af69efabcbfbfb8f176109a
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-11 04:03:54 -0800

    Task6

---
+++ python-classes/6-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""string"""
+
+
+class Square:
+    """size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int or type(value[1]…
