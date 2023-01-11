c286e6d6aef2d52910c77eed92458a8f208140f9
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-11 03:53:12 -0800

    Task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Square Executable file"""
+"""string"""
 
 
 class Square:
-"""Defines a square"""
+    """string size"""
 
-
-    def __init__(self, size=0):
-        """Initialises the data"""
+    def _init_(self, size=0):
         self.size = size
-
-    def area(self):
-        """Returns current square area"""
-        return self.__size**2
 
     @property
     def size(self):
-        """Getter method"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method"""
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self._size * self._size
