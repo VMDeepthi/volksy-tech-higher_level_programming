91e5aff59373b323e019239ec35a1b8e9d99092e
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-13 00:04:11 -0800

    Task8

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""string"""
+
+
 class Square:
     """Defines a square"""
+
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialises the data"""
         self.size = size
