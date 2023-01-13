cbc13e5e881182386f2a0a13ba219eeec461b6e8
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-12 22:04:35 -0800

    Task6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Unittest for max_integer([ ])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
@@ -30,4 +30,4 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1]), 1)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.maini()
