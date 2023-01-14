cd3049d365fdafe734b4be655e1a2a8860a75c6a
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-13 23:53:05 -0800

    Task11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
+"""
+    class Square that inherits from Rectangle (9-rectangle.py).
+    (task based on 10-square.py).
+    Instantiation with size: def __init__(self, size)::
+        size must be private. No getter or setter
+        size must be a positive integer, validated by
+        integer_validator
+    the area() method must be implemented
+    print() should print, and str() should return, the
+    square description: [Square] <width>/<height>
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
+    """ class square inherits from rectangle class """
 
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
+        """ initializes square """
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        """ returns area of square """
+        return super().area()
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """ returns [Square] <width>/<height> string """
+        a = str(self.__size)
+        return "[Square] " + a + "/" + a
