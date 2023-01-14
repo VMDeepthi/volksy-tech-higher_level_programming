4a2257536e34a3bf939866377299257d19ab5ea4
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-14 08:06:12 -0800

    Task10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
+"""
+    class Square that inherits from Rectangle (9-rectangle.py):
+    Instantiation with size: def __init__(self, size)::
+        size must be private. No getter or setter
+        size must be a positive integer, validated by
+        integer_validator
+    the area() method must be implemented
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
+    """ class square inherits from rectangle class """
 
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
+        """ initializes square """
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
