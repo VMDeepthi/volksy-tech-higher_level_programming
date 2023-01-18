181de28d95fc8a00daaea06eecf6f15f1e3f04c5
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-18 04:46:02 -0800

    Task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,6 @@ import sys
 from sys import argv
 
 
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
