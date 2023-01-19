f9bf473b8f5254c233c463b966564deefd48f515
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-18 23:02:26 -0800

    Task4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
-"""text indent"""
+"""
+    Insert here module comment
+
+    Write a function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+
+    Prototype: def text_indentation(text):
+
+    text must be a string, otherwise raise a TypeError
+    exception with the message text must be a string
+    There should be no space at the beginning or at the
+    end of each printed line so that you don’t miss any
+    edge case
+
+"""
 
 
 def text_indentation(text):
-    """text indent"""
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    """ insert doble jump line after . : or ? """
+    str_error = "text must be a string"
+    new_text = ""
+    flag = False
+    if type(text) is not str:
+        raise TypeError(str_error)
+    new_text = text.replace(". ", ".")
+    new_text = new_text.replace(": ", ":")
+    new_text = new_text.replace("? ", "?")
+    for char in new_text:
+        if char in [".", "?", ":"]:
+            print(char)
+            print()
+            flag = True
+        else:
+            if flag is False:
+                print(char, end="")
+            else:
+                if char != " ":
+                    print(char, end="")
+                    flag = False
