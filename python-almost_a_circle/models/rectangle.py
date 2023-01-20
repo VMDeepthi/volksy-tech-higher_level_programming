c39ec6663a96f30dee88904a2c4d84ccdcd852ce
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-20 10:53:46 -0800

    Task4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
+
+    def area(self):
+        """return area of rectangle"""
+        return self.width * self.height
