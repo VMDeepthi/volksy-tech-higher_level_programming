1b7e7bb936117ce4a3bc61b1ad80f2bbb8eb8f80
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-20 10:24:26 -0800

    Task3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,9 +25,13 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width"""
-        self.__width = value
-
-    @property
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+   @property
     def height(self):
         """height"""
         return self.__height
@@ -35,8 +39,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height"""
-        self.__height = value
-
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
     @property
     def x(self):
         """x"""
