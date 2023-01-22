50fddf6e9c89d569bb4a2454e220f741c40767e5
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-22 02:11:42 -0800

    Task10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,6 +5,6 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Square class"""
-    def _init_(self, size, x=0, y=0, id=None):
+    def __init__(self, size, x=0, y=0, id=None):
         """Initializer for Square class"""
-        super()._init_(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
