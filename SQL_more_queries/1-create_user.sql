fb13bd049f4319243cb492f398b3c7201fe18718
VMDeepthi <deepthim@brightcomgroup.com>
2023-01-31 03:02:07 -0800

    Task1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+    IDENTIFIED BY 'user_0d_1_Mohana@123';
 GRANT ALL PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+   IDENTIFIED BY 'user_0d_1_Mohana@123';
 FLUSH PRIVILEGES;
