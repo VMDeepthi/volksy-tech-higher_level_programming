6f145d1bbc307ee54e458be83e4df0e6eddccecd
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-07 08:18:39 -0800

    Task7

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,44 +1,6 @@
 #!/usr/bin/python3
 """
-    Write a function that multiplies 2 matrices:
-
-    Read: Matrix multiplication - only Matrix product (two matrices)
-
-    Prototype: def matrix_mul(m_a, m_b):
-
-    m_a and m_b must be validated with these requirements in this order
-
-    m_a and m_b must be an list of lists of integers or floats:
-
-    1. if m_a or m_b is not a list: raise a TypeError exception
-    with the message m_a must be a list or m_b must be a list
-
-    2. if m_a or m_b is not a list of lists: raise a TypeError
-    exception with the message m_a must be a list of lists or
-    m_b must be a list of lists
-
-    3. if m_a or m_b is empty (it means: = [] or = [[]]): raise
-    a ValueError exception with the message m_a can't be empty
-    or m_b can't be empty
-
-    4. if one element of those list of lists is not an integer or
-    a float: raise a TypeError exception with the message m_a
-    should contain only integers or floats or m_b should
-    contain only integers or floats
-
-    5. if m_a or m_b is not a rectangle (all ‘rows’ should be
-    of the same size): raise a TypeError exception with the
-    message each row of m_a must be of the same size or
-    each row of m_b must be of the same size
-
-    6. If m_a and m_b can’t be multiplied: raise a ValueError
-    exception with the message m_a and m_b can't be
-    multiplied
-    #a = [[3, 2, 1, 5], [9, 1, 3, 0]]
-    #b = [[2, 9, 0], [1, 3, 5], [2, 4, 7], [8, 1, 5]]
-    #print(mult(a, b))
-    # 3x2
-    #axb = [[60 60]]
+    Write a function that mul
 """
 
 
