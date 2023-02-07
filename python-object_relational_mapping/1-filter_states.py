8bfc023669c60055db8c6d5aa452cab2301d69a6
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-06 22:16:20 -0800

    Task1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""Filter States"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(username=sys.argv[1], passwordd=sys.argv[2], databasename=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(states) for states in c.fetchall() if states[1][0] == "N"]
