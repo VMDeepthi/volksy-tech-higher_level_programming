7fe18ef9ae4fa63a1d82d260e77bd4509033d19e
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-07 07:37:06 -0800

    Task3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""safe filter"""
+"""safe filter states"""
 
 
 import sys
-import MYSQLdb
+import MySQLdb
 
-if__name__="__main__":
-    db=MYSQL.db=connect(user=sys.argv[1] ,passwd=sys.argv[2],db=sys.argv[3])
-    c=db.cursor()
-    c.execute("SELECT * FROM 'states'")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]    
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
