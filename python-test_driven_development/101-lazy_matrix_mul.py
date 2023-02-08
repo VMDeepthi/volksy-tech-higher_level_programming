26bdf55713073f6083c633a8fd7f2a1ccaa75ff7
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-07 20:15:40 -0800

    Task7

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-    Module to find the max integer in a 
-"""
 
-
-import numpy as np
+"""
+Multiplies 2 matrices by using 'numpy' module.
+"""
+import numpy as lazy
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """ multiply two matrix with numpy """
-    return np.dot(m_a, m_b)
+    """
+    Function to multiplies 2 matrices
+    """
+    return lazy.matmul(m_a, m_b)
