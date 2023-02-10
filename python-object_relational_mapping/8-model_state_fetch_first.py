c8bc6585d235ca595d1d4c80c6b78dd7441b85c0
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-10 01:09:54 -0800

    Task8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""A script lists all State objects from the database hbtn_0e_6_usa"""
-
+"""List first State objects from db"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+def list_first_state_obj():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        print("{:}: ".format(session.query(State).order_by(
-            State.id).first().id), end="")
-        print(session.query(State).order_by(State.id).first().name)
-    except:
+
+    session = Session(engine)
+
+    f = session.query(State).first()
+
+    if f:
+        print("{}: {}".format(f.__dict__['id'], f.__dict__['name']))
+    else:
         print("Nothing")
+
     session.close()
+
+
+if __name__ == "__main__":
+    list_first_state_obj()
