0e690c790c015ed772843ffc3c806ebc40b9a788
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-10 01:30:03 -0800

    Task11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""Inserts into State obj from db"""
+"""A script lists all State objects from the database hbtn_0e_6_usa"""
+
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
-def insert_to_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    state = State(name='Louisiana')
-
-    session.add(state)
-    session.commit()
-    print(state.id)
-
-    session.close()
-
-
 if __name__ == "__main__":
-    insert_to_state_obj()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+    session.close()
