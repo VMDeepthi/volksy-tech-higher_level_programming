efc70cada95e97c0958475f31b131f8f72ca3262
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-13 22:20:51 -0800

    Task16

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""script to create city and state object"""
+''' creates State "California" with City "San Francisco" '''
+
 
 if __name__ == "__main__":
-    from sqlalchemy.engine import create_engine
-    from sqlalchemy.engine.url import URL
-    from sqlalchemy.orm import Session
-    from relationship_city import City
-    from relationship_state import Base, State
     from sys import argv
-
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
-
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state in session.query(State):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_list = session.query(State).order_by(State.id).all()
+    for state in state_list:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
     session.close()
