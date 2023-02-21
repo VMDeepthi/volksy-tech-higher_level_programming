29e1509af1826d23339d54df15c5a20daabf3b1e
VMDeepthi <deepthim@brightcomgroup.com>
2023-02-21 02:53:42 -0800

    Task6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < strings.length; i++) {
   console.log(strings[i]);
